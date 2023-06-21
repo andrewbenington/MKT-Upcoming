68902ac2ca19d4d4640e25797de9442c53723179
Andrew Benington <andrewbenington57@gmail.com>
2023-06-21 10:06:45 -0500

    squeaky clean sprint

--- src/consts/existingAssets.ts
+++ src/consts/existingAssets.ts
@@ -1,12 +1,9 @@
 const existingAssets = [
-  'GBALuigiCircuit',
-  'GCNWaluigiStadium',
   'TourBathroom',
   'TourDK',
   'TourMD',
   'TourMU',
   'TourNU',
   'TourOR',
-  'TourYI',
 ];
 export default existingAssets;
